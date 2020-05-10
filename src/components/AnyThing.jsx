64ca5e88e0e9bb4d65bdcf46abd6ff6135ffd8d4
JuankaMF <juancarlos69528125@gmail.com>
2020-05-10 12:59:07 -0400

    added ESlint in my project

---
+++ src/components/AnyThing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const AnyThing = () => {
+    return (
+        <h1>hi! i'm anything</h1>
+    );
+}
+
+export default AnyThing;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import AnyThing from './components/AnyThing'
+
+ReactDOM.render(<AnyThing />, document.getElementById('app'))
